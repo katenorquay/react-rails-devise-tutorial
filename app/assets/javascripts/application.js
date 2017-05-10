9db0802a71ff82cd313dfa64ba746b7ca4d19739
katenorquay <katenorquay@live.com>
2017-05-10 16:44:44 +1200

    welcome react component loading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,5 @@
 //= require jquery_ujs
 //= require react
 //= require react_ujs
-//= require components
 //= require_tree .
+//= require components 
---
+++ app/assets/javascripts/components.jsx
@@ -0,0 +1,11 @@
+var Welcome = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Welcome!</h1>
+        <a href='/users/sign_up'>Sign Up!</a>
+        <a href='/users/sign_in'>Sign In!</a>
+      </div>
+    )
+  }
+});
